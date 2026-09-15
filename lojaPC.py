dbc6cd45d7896e1461c633455d71e44a761d0e57
Kauã Santana <kauasilvarino@gmail.com>
2026-09-14 23:15:19 -0300

    comentário do código

--- lojaPC.py
+++ lojaPC.py
@@ -2,23 +2,23 @@ print("="*20)
 print(">>> LOJA PCISTA <<<")
 print("="*20)
 continuar = barato = ""
-tot = caro = menor = cont = 0
+tot = caro = menor = cont = 0            #declarando variáveis
 
-while continuar != "N":
+while continuar != "N":                  #enquanto quiser continuar for igual(==) "S" (Sim):
     produto = input("Nome do produto: ")
     preco = float(input("Preço: R$"))
     continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
-    cont += 1
-    tot += preco
-    if preco > 1000:
+    cont += 1                            
+    tot += preco                         #total de toda compra, recebe(=) soma de qualquer valor informado dentro do while
+    if preco > 1000:                     
         caro += 1
-    if cont == 1:
-        menor = preco
-        barato = produto
+    if cont == 1:                        #enquanto for informado apenas 1 produto, ele é o menor (e maior),
+        menor = preco                    #menor recebe(=) preco, porque é o único informado
+        barato = produto                 #barato recebe(=) produto, porque é o único informado
     else:
-        if preco < menor:
-            menor = preco
-            barato = produto
+        if preco < menor:                #se tiver mais de um produto informado, e o próximo do laço for menor, o próximo passa a ser o menor.
+            menor = preco                #o preço desse produto passa recebe(=) menor
+            barato = produto             #o nome do produto recebe(=) barato
 
 print("------- FIM DO PROGRAMA -------")
 print(f"O total da compra foi R${tot:.2f}")
